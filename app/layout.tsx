cb38b045a82fe2991f697425e153a78867686b10
Pranav-Official <pranavhtjp07@gmail.com>
2023-09-03 11:03:34 +0530

    2nd

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Sulphur+Point:wght@300;400;700&display=swap" rel="stylesheet"></link>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row bg-background text-text ">
-      <div className='flex flex-row h-28 w-screen'>
-        <Image className='ml-32 py-7 px-3' src="/mazelogo.png" alt="maze" width={80} height={80} />
-        <h1 className='text-7xl py-5 px-3'>MazeVisualizer</h1>
-        <button className='flex flex-row align-middle justify-center bg-primary my-7 px-4 rounded-lg mr-32 ml-auto'>
-          <p className='py-4 px-2 font-Poppins'>GitHub</p>
-          <Image className ="py-3" src="/github.png" alt="maze" width={30} height={30} />
-        </button>
+    <div className="flex min-h-screen flex-col mx-20 2xl:mx-32 bg-background text-text font-Sulphur_point border-2">
+      <div className='flex flex-row h-28 w-screen'> {/* the top navbar*/}
+        <Image className=' py-6 pr-3 ' src="/mazelogo.png" alt="maze" width={80} height={80} />
+        <h1 className='text-7xl py-6 px-3'>MazeVisualizer</h1>
+        <div>
+          <button className='flex flex-row align-middle justify-center bg-primary rounded-lg  '>
+            <p className='py-4 px-2 text-lg font-bold'>GitHub</p>
+            <Image className ="py-3" src="/github.png" alt="maze" width={30} height={30} />
+          </button>
+        </div>
       </div>
-    </main>
+      <div>
+        hero
+      </div>
+    </div>
   )
 }
